31ed699740aa35ddc25b90653d21eaca6ae5a5d4
Gregoire Layet <git@gregoirelayet.com>
2024-04-07 16:32:22 +0200

    🎨 django project refactoring + async server setup

---
+++ src/config/settings/dev.py
@@ -0,0 +1,14 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-s$fe++l8)hr1&k@^r3t!lw79w(hacm(&&oucvx4+@kl&bnq*2g"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+ASGI_APPLICATION = "config.asgi_dev.app"
