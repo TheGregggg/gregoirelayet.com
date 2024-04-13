083e94cb2c771c9221af8be11cf29092383e8542
Gregoire Layet <git@gregoirelayet.com>
2024-04-13 22:41:24 +0200

    🔥 Better js code for theme feature

--- src/components/theme_switcher/script.js
+++ src/components/theme_switcher/script.js
@@ -1,21 +1,6 @@
-function set_theme_to_html(theme) {
+function set_theme(theme) {
 	document.documentElement.dataset.theme = theme;
-}
-function save_theme_setting(theme) {
 	localStorage.setItem("theme", theme);
-	document.getElementById("theme-switcher").dataset.currentThemeSetting =
-		theme;
-}
-
-function auto_theme_set_theme() {
-	if (localStorage.getItem("theme") != "auto") {
-		return;
-	}
-	if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-		set_theme_to_html("dark");
-	} else {
-		set_theme_to_html("light");
-	}
 }
 function theme_switcher() {
 	theme_setting = localStorage.getItem("theme");
@@ -34,32 +19,16 @@ function theme_switcher() {
 		default:
 			return;
 	}
-	save_theme_setting(new_theme);
-	set_theme_to_html(new_theme);
+	set_theme(new_theme);
 
-	if (new_theme == "auto") {
-		auto_theme_set_theme();
-	}
 }
 
 function init_theme() {
 	theme_setting = localStorage.getItem("theme");
-	if (theme_setting == null) {
+	if (theme_setting == null || ["dark", "light", "auto"].includes(theme_setting) == false) {
 		theme_setting = "auto";
-		localStorage.setItem("theme", theme_setting);
 	}
-
-	if (theme_setting == "auto") {
-		auto_theme_set_theme();
-	} else if (theme_setting == "light" || theme_setting == "dark") {
-		set_theme_to_html(theme_setting);
-	}
-
-	window
-		.matchMedia("(prefers-color-scheme: dark)")
-		.addEventListener("change", auto_theme_set_theme, true);
-
-	save_theme_setting(theme_setting);
+	set_theme(theme_setting)
 }
 
 (function () {
