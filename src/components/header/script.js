b7d2222cccd0dd326944f594984b511d2ed5f067
Gregoire Layet <git@gregoirelayet.com>
2024-04-15 23:25:55 +0200

    ✨ 🎨 implemented section aboutme on home page | needed to rework base margin system

--- src/components/header/script.js
+++ src/components/header/script.js
@@ -1,4 +1,9 @@
-(function () {
-	header_title = document.getElementById("main-title");
+function onResize() {
+	let header_title = document.getElementById("main-title");
 	header_title.style.width = header_title.firstChild.scrollWidth + "px";
+}
+
+(function () {
+	window.addEventListener("load", onResize);
+	window.addEventListener("resize", onResize);
 })();
