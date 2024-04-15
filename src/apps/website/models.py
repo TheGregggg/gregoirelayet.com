05412466bb504f1256ba5cf33c7b5b10620da2a1
Gregoire Layet <git@gregoirelayet.com>
2024-04-15 21:23:13 +0200

    ♻️ Setup home page with wagtail

--- src/apps/website/models.py
+++ src/apps/website/models.py
@@ -1,2 +1,32 @@
-from django.db import models
-from django_stubs_ext.db.models import TypedModelMeta
+from .base import HtmxPage
+from wagtail import blocks
+from wagtail.fields import RichTextField, StreamField
+from wagtail.admin.panels import FieldPanel
+
+
+class TimelineBlock(blocks.StructBlock):
+    title = blocks.CharBlock()
+    year = blocks.CharBlock()
+
+    class Meta:
+        icon = "calendar-alt"
+
+
+class PassionBlock(blocks.StructBlock):
+    title = blocks.CharBlock()
+    content = blocks.RichTextBlock()
+
+    class Meta:
+        icon = "pick"
+
+
+class HomePage(HtmxPage):
+    about_me = RichTextField(blank=True)
+    timeline = StreamField([("Event", TimelineBlock())])
+    passions = StreamField([("Passion", PassionBlock())])
+
+    content_panels = HtmxPage.content_panels + [
+        FieldPanel("about_me"),
+        FieldPanel("timeline"),
+        FieldPanel("passions"),
+    ]
