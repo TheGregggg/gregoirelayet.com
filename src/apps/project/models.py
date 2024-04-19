94d8b2356c811b54910e81fcd7b0c58f66174a23
Gregoire Layet <git@gregoirelayet.com>
2024-04-19 20:29:01 +0200

    starting working on project page

---
+++ src/apps/project/models.py
@@ -0,0 +1,8 @@
+from apps.website.base import HtmxPage
+from wagtail import blocks
+from wagtail.fields import RichTextField, StreamField
+from wagtail.admin.panels import FieldPanel
+
+
+class ProjectsPage(HtmxPage):
+    pass
