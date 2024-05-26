9f7d6b5915275803ec6585d7375654093c9d7f37
Gregoire Layet <git@gregoirelayet.com>
2024-05-26 17:49:21 +0200

    creating project component

---
+++ src/components/project/project.py
@@ -0,0 +1,16 @@
+from django_components import component
+from apps.project.models import ProjectPage
+
+
+@component.register("project")
+class Project(component.Component):
+    template_name = "project/template.html"
+
+    def get_context_data(self, project: ProjectPage):
+        return {
+            "project": project,
+        }
+
+    class Media:
+        css = "project/style.css"
+        js = "project/script.js"
