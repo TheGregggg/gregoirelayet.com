65ad8ae4c062abc51e5a5f282ddc426e53ca9a13
Gregoire Layet <git@gregoirelayet.com>
2024-06-11 01:26:34 +0200

    better  controll on setup db script

---
+++ db/create_db.sql
@@ -0,0 +1,12 @@
+CREATE ROLE gerg WITH LOGIN PASSWORD 'gerg_pass';    
+
+--- Django user recommendation
+ALTER ROLE gerg SET client_encoding TO 'utf8';
+ALTER ROLE gerg SET default_transaction_isolation TO 'read committed';
+ALTER ROLE gerg SET timezone TO 'UTC';
+
+CREATE DATABASE "gregoirelayet.com";   
+GRANT ALL PRIVILEGES ON DATABASE "gregoirelayet.com" TO gerg;
+
+CREATE DATABASE "test_gregoirelayet.com";
+GRANT ALL PRIVILEGES ON DATABASE "test_gregoirelayet.com" TO gerg;
---
+++ set_db.sh
@@ -0,0 +1,2 @@
+docker exec gregoirelayetcom-db-1 psql -U gerg_db_admin -d postgres -f /var/lib/postgresql/sql_script/create_db.sql
+docker exec gregoirelayetcom-db-1 psql -U gerg -d gregoirelayet.com -f /var/lib/postgresql/sql_script/setup_db.sql
