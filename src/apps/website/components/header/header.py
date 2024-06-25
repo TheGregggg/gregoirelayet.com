5627ffbf511a781f795a49175f0e9d2ddb76afa1
Gregoire Layet <git@gregoirelayet.com>
2024-06-25 22:40:07 +0200

    🎨 implemente header singleton's setting for selecting header pages

--- src/apps/website/components/header/header.py
+++ src/apps/website/components/header/header.py
@@ -1,9 +1,15 @@
 from django_components import component
+from apps.website.models.components.header import Header as HeaderModel
+from django.http import HttpRequest
 
 
 @component.register("header")
 class Header(component.Component):
     template_name = "header/template.html"
+
+    def get_context_data(self, request: HttpRequest):
+        data = HeaderModel.load(request_or_site=request)
+        return {"data": data}
 
     class Media:
         css = "header/style.css"
