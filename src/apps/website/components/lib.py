955c4a26b373926d6f8446ce6317be864ffdebf1
Gregoire Layet <git@gregoirelayet.com>
2024-06-28 17:15:44 +0200

    ✏️ update website component lib imports

--- src/apps/website/components/lib.py
+++ src/apps/website/components/lib.py
@@ -1,3 +1,4 @@
 import apps.website.components.footer.footer
 import apps.website.components.header.header
 import apps.website.components.other_passion.other_passion
+import apps.website.components.experience.experience
