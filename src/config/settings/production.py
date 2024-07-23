5a500de63a2bed305f9488605527188e4a676003
Gregoire Layet <git@gregoirelayet.com>
2024-07-23 18:37:02 +0200

    🔒️ setting up django settings from env var

--- src/config/settings/production.py
+++ src/config/settings/production.py
@@ -1,25 +1,28 @@
 import os
 from pathlib import Path
 
+from decouple import config
+
 from config.settings.base import *  # noqa
 
 DEBUG = False
 
-with open(
-    os.path.join(Path(__file__).resolve().parent.parent, "secrets/secret.txt")
-) as f:
+SECRET_KEY_FILE = config(
+    "SECRET_KEY_FILE",
+    default=os.path.join(Path(__file__).resolve().parent.parent, "secrets/secret.txt"),
+)
+with open(SECRET_KEY_FILE, "r") as f:
     SECRET_KEY = f.read().strip()
 
-with open(os.path.join("/", "run", "secrets", "db_user_password")) as f:
+with open(config("DB_PASSWORD_FILE"), "r") as f:
     DATABASES["default"]["PASSWORD"] = f.read().strip()  # noqa
-DATABASES["default"]["HOST"] = os.environ.setdefault("DB_HOSTNAME", "")  # noqa
-DATABASES["default"]["PORT"] = os.environ.setdefault("DB_PORT", "")  # noqa
 
-DNS_NAME = "gregoirelayet.com"
+DATABASES["default"]["HOST"] = config("DB_HOST")  # noqa
+DATABASES["default"]["PORT"] = config("DB_PORT")  # noqa
 
-ALLOWED_HOSTS = [DNS_NAME, "127.0.0.1"]
-CSRF_TRUSTED_ORIGINS = [f"https://{DNS_NAME}"]
+FQDN = config("DNS_NAME", default="127.0.0.1")
 
-WSGI_APPLICATION = "config.wsgi.app"
-
-COMPRESS_ENABLED = True
+ALLOWED_HOSTS = [
+    FQDN,
+]
+CSRF_TRUSTED_ORIGINS = [f"https://{FQDN}"]
