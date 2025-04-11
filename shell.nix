212c864a6a73910f501da1ff7500a5956288fc67
Grégoire Layet <git@gregoirelayet.com>
2025-04-11 23:22:31 +0200

    add minhtml and pigz to nix shell package

--- shell.nix
+++ shell.nix
@@ -1,8 +1,41 @@
-let
-  pkgs = import <nixpkgs> {};
+{pkgs ? import <nixpkgs> {}}: let
+  minhtmlRepo = pkgs.fetchFromGitHub {
+    owner = "wilsonzlin";
+    repo = "minify-html";
+    rev = "2301223773dadce30a33b4c407d8b2524adeb5e2"; # You can change this to a specific commit or tag
+    sha256 = "sha256-346S5Yl8140B7Bnn1hdtYRHqssgGEWCEvbiBkPXrDeg="; # Placeholder for the actual hash, will be replaced
+  };
 in
   pkgs.mkShell {
-    packages = [
-      pkgs.zola
+    buildInputs = [
+      pkgs.rustc
+      pkgs.cargo
     ];
+
+    packages = with pkgs; [
+      zola
+      pigz
+    ];
+
+    shellHook = ''
+      export PATH=$PATH:$HOME/.cargo/bin
+      echo "🔧 Entered dev shell for 'minhtml'"
+      # Download the repo and install the crate if not already installed
+      if ! command -v minhtml >/dev/null; then
+        echo "🚀 Installing minhtml with cargo..."
+
+        # # Create a temporary folder to unpack the repo
+        tmpDir=$(mktemp -d)
+
+        # Go into the crate directory
+        cd ${minhtmlRepo}/minhtml
+
+        # Run cargo install
+        cargo install --path . --force --target-dir $tmpDir
+
+        cd -  # Go back to the original directory
+      else
+        echo "✅ minhtml already installed in ~/.cargo/bin"
+      fi
+    '';
   }
