97cdf0c327d480f5c6b69f11d7521be119bc368d
Grégoire Layet <git@gregoirelayet.com>
2025-04-14 10:22:17 +0200

    tweaking html minification + resolving bug

--- ssg/build_min_comp.sh
+++ ssg/build_min_comp.sh
@@ -5,15 +5,15 @@ dst="dest"
 
 cp -R "$src"/. "$dst" && cd "$dst"
 
+# minhtml --keep-closing-tags --keep-html-and-head-opening-tags  $(find . -type f -a -name "*\.html" -o -name "*\.xml" ) 
 
-minhtml $(find . -type f -a -name "*\.html" -o -name "*\.xml" ) 
 css_files=$(find . -type f -name "*\.css" )
 echo "$css_files" | while read -r file
 do
 	lightningcss --minify  "$file" -o  "$file"
 done
 
-files=$(find . -type f -a -name "*\.html" -o -name "*\.css" -o -name "*\.js" -o -name "*\.xml" )
+files=$(find . -type f -a -name "*\.html" -o -name "*\.css" -o -name "*\.js" -o -name "*\.xml" -o -name "*\.ttf" )
 echo "$files" | while read -r file
 do
 	gzip -9 < "$file" > "$file.gz"
